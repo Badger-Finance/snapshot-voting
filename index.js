fbefd13eb0459de3e793fbaa844683199fe6cd7f
Ayush Shukla <shuklaayush247@gmail.com>
2021-09-29 00:40:53 +0530

    fix: sign message through wallet/pk for voting

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const vote = async () => {
-  const {Wallet, Contract} = require("ethers")
+const { Client: OriginalClient } = require("@snapshot-labs/snapshot.js");
+const { Wallet } = require("@ethersproject/wallet");
 
-  // Pk is empty, no worries
-  PRIVATE_KEY = process.env.PRIVATE_KEY || "511551aa8d8975bdd308282dedb7aca2a6f0cfcd919ba844b94eca7addceae9f"
-  const wallet = new Wallet(PRIVATE_KEY)
-  const snapshot = require('@snapshot-labs/snapshot.js')
+// TODO: Ideally should come from snapshot.js
+const VERSION = "0.1.3";
 
-  const client = new snapshot.Client("https://hub.snapshot.org/")
+class Client extends OriginalClient {
+  constructor(address) {
+    super(address);
+  }
 
-  client.vote(wallet, await wallet.getAddress(), "cvx.eth", {proposal: 18, choice: true, metadata: {}})
+  async voteViaWallet(wallet, space, { proposal, choice, metadata = {} }) {
+    try {
+      const msg = {
+        address: await wallet.getAddress(),
+        msg: JSON.stringify({
+          version: VERSION,
+          timestamp: (Date.now() / 1e3).toFixed(),
+          space,
+          type: "vote",
+          payload: { proposal, choice, metadata },
+        }),
+      };
+      msg.sig = await wallet.signMessage(msg.msg);
+      // console.log(msg);
+      return await this.send(msg);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 
-vote()
+const vote = async () => {
+  // Pk is empty, no worries
+  PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "511551aa8d8975bdd308282dedb7aca2a6f0cfcd919ba844b94eca7addceae9f";
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  const client = new Client();
+
+  client.voteViaWallet(wallet, "cvx.eth", {
+    proposal: 18,
+    choice: true,
+  });
+};
+
+vote();
